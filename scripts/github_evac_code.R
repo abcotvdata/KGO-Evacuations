a2c497b0f0d8dfce59b78e62e628eb7b69dd52b4
lindseyf96 <39309451+lindseyf96@users.noreply.github.com>
2022-10-11 15:39:02 -0700

    Update github_evac_code.R

--- scripts/github_evac_code.R
+++ scripts/github_evac_code.R
@@ -14,7 +14,7 @@ library(fontawesome)
 #GOOGLE_JSON <- Sys.getenv("GOOGLE_JSON")
 
 #drive_auth(token = GOOGLE_JSON)
-drive_auth(path = "src/google_service_account.json")
+drive_auth(path = "/Users/runner/work/KGO-Evacuations/KGO-Evacuations/google_service_account.json")
 #gs4_auth(token = GS_API)
 
 # loading in each google sheet and adding county + state columns
